ccae74c412f43db46579a343ebdefbac7d65dc96
Chin Tzulin <jp20316@w022341412910m.local>
2015-11-10 12:15:17 +0900

    Fix import path

--- g2p/g2p.py
+++ g2p/g2p.py
@@ -17,7 +17,7 @@ import requests
 import logging
 import yaml
 
-from actions.action import *
+from g2p.actions.action import *
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from urlparse import urlparse, parse_qs
 
